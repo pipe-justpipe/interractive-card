c4a3b9f60c1ac8dd7c3f1aff144f13f529b5b1b6
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-09-13 22:23:26 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       colors:{
         'custom-voilet':'hsl(278, 68%, 11%)'
       },  
+      backgroundImage: theme => ({
+        'background1': "url('../images/bg-main-desktop.png')",
+        'background2': "url('../images/bg-main-mobile.png')"
+      })
     },
   }, 
   plugins: [],
